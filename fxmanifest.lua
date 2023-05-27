a1b78790073dea83edfaf75f653286309c972c27
Matt <31101519+Kronus8@users.noreply.github.com>
2023-05-27 12:01:55 +0100

    Add Lua 5.4 support

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 author ''
 description ''
+lua54 'yes'
 
 client_scripts {
     'client/*.lua',
